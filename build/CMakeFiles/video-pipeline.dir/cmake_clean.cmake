3ad16f5a1ba4574a27ec067d259bf40fda0ccf85
mitko-kinov <kinov.mitko@gmail.com>
2025-02-21 17:19:11 +0100

    vcpkg installed

---
+++ build/CMakeFiles/video-pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video-pipeline.dir/src/main.cpp.o"
+  "CMakeFiles/video-pipeline.dir/src/main.cpp.o.d"
+  "video-pipeline"
+  "video-pipeline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video-pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fmt/core.h>
+
+int main() {
+	std::cout << fmt::format("Hello, vcpkg!\n");
+	return 0;
+}
